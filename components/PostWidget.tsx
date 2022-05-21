5cb89c8171a731e3f0ebd8eb1e33f46f03553ddc
Sunilshah-7 <sunilshah2416@gmail.com>
2022-05-21 09:41:18 +0545

    Up to Individual post detail

--- components/PostWidget.tsx
+++ components/PostWidget.tsx
@@ -17,7 +17,34 @@ const PostWidget = ({ categories, slug }: { categories: any; slug: any }) => {
   }, [slug])
 
   console.log(relatedPosts)
-  return <div>PostWidget</div>
+  return (
+    <div className="p-8 mb-8 bg-white rounded-lg shadow-lg">
+      <h3 className="pb-4 mb-8 text-xl font-semibold border-b">
+        {slug ? 'Related Posts' : 'Recent Posts'}
+      </h3>
+      {relatedPosts.map((post: any) => (
+        <div className="flex items-center w-full mb-4" key={post.title}>
+          <div className="flex-none w-16">
+            <img
+              alt={post.title}
+              height="60px"
+              width="60px"
+              className="align-middle rounded-full"
+              src={post.featuredImage.url}
+            />
+          </div>
+          <div className="flex-grow ml-4">
+            <p className="text-gray-500 font-xs">
+              {moment(post.createdAt).format('MMMM Do YYYY')}
+            </p>
+            <Link key={post.title} href={`/post/${post.slug}`}>
+              {post.title}
+            </Link>
+          </div>
+        </div>
+      ))}
+    </div>
+  )
 }
 
 export default PostWidget
